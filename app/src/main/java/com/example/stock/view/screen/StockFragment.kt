2375bc74a829b3d97a72944691812c9d2b749f37
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-12-21 11:24:24 +0900

    ➕ 불필요한 dependency 제거, shimmer 라이브러리 추가

--- app/src/main/java/com/example/stock/view/screen/StockFragment.kt
+++ app/src/main/java/com/example/stock/view/screen/StockFragment.kt
@@ -2,11 +2,10 @@ package com.example.stock.view.screen
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.stock.data.RetrofitClient
