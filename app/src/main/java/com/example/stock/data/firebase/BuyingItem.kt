99d52e79e6c86326d49340c83c891e365a48fb9e
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-01-10 17:14:13 +0900

    :lipstick: :: 주식 구매 다이얼로그 수정, 주식 구매 데이터 클래스 생성

---
+++ app/src/main/java/com/example/stock/data/firebase/BuyingItem.kt
@@ -0,0 +1,7 @@
+package com.example.stock.data.firebase
+
+data class BuyingItem(
+    val itemName: String = "",
+    val buyingCount: Int = 0,
+    val price: Int = 0
+)
--- app/src/main/java/com/example/stock/view/dialog/BuyStockDialog.kt
+++ app/src/main/java/com/example/stock/view/dialog/BuyStockDialog.kt
@@ -9,14 +9,22 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
+import com.example.stock.data.firebase.BuyingItem
+import com.example.stock.data.firebase.User
+import com.example.stock.data.response.stock.Item
 import com.example.stock.databinding.DialogBuyStockBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-class BuyStockDialog(context: Context, private val price: String, private val itemName: String): Dialog(context) {
+class BuyStockDialog(context: Context, private val item: Item): Dialog(context) {
     private lateinit var binding: DialogBuyStockBinding
     private val auth = FirebaseAuth.getInstance()
     private val database = FirebaseDatabase.getInstance().reference
+
+    private val clpr = item.clpr.toInt()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +35,12 @@ class BuyStockDialog(context: Context, private val price: String, private val it
     }
 
     private fun initView() = with(binding) {
-        val intPrice = price.substring(0, price.length -1).toInt()
+//        val intPrice = clpr.substring(0, clpr.length -1).toInt()
 
         setCancelable(false)    // 빈 공간 클릭해도 다이얼로그 안없어지게
         window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-        dialogTitle.text = "${itemName}을(를) 구매하시겠습니까?"
+        dialogTitle.text = "${item.itmsNm}을(를) 구매하시겠습니까?"
         // 텍스트가 변경될때마다 호출
         stockCount.addTextChangedListener(object :TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -42,7 +50,7 @@ class BuyStockDialog(context: Context, private val price: String, private val it
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 if (stockCount.text.toString().isEmpty()) {
                     totalPrice.text = "총 0원"
-                } else totalPrice.text = "총 ${stockCount.text.toString().toInt() * intPrice}원"
+                } else totalPrice.text = "총 ${stockCount.text.toString().toInt() * clpr}원"
             }
 
             override fun afterTextChanged(p0: Editable?) {
@@ -51,17 +59,42 @@ class BuyStockDialog(context: Context, private val price: String, private val it
         })
 
         buyStock.setOnClickListener {
-            database.child("user").child(auth.currentUser?.uid.toString())
-                .child("money").setValue(stockCount.text.toString().toInt() * intPrice).addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(context, "구매가 완료되었습니다.", Toast.LENGTH_SHORT).show()
-                        dismiss()
-                    }
-                }
+            // 유저 돈 값 변경, 매수 목록에 추가
+            val totalPrice = stockCount.text.toString().toInt() * clpr
+            changeMoney(totalPrice)
         }
 
         cancelBuy.setOnClickListener {
             dismiss()
         }
+    }
+
+    private fun changeMoney(total: Int) {
+        val user = auth.currentUser?.uid.toString()
+        database.child("user").child(user).addListenerForSingleValueEvent(object :ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.getValue(User::class.java)
+                val res = value?.money?.minus(total)
+
+                database.child("user").child(user)
+                    .child("money").setValue(res).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            addBuyingList(total)
+                        }
+                    }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
+    }
+
+    private fun addBuyingList(total: Int) = with(binding) {
+        val buyingItem = BuyingItem(item.itmsNm, stockCount.text.toString().toInt(), total)
+        database.child("buying").child(auth.currentUser?.uid.toString()).child(item.itmsNm).setValue(buyingItem)
+            .addOnSuccessListener {
+                Toast.makeText(context, "구매가 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                dismiss()
+            }
     }
 }
